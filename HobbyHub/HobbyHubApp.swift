7c6b1163c3e3d653fcdc94bce36da80447399cb2
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2023-10-09 11:35:56 +0200

    Initial Commit

---
+++ HobbyHub/HobbyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyHubApp.swift
+//  HobbyHub
+//
+//  Created by Андрей Банин on 9.10.23..
+//
+
+import SwiftUI
+
+@main
+struct HobbyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
